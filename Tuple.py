b90ca07d4940a5719654dcb8575942b5cb40cfba
PrasetyoTeddy <38429659+PrasetyoTeddy@users.noreply.github.com>
2018-04-17 15:24:55 +0700

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,21 @@
+tupA=(0,1,2,3)
+print tupA
+
+tupB=(4,5,6,7)
+print tupB
+
+tupC=tupA+tupB
+print tupC
+
+tupD=tupC*2
+print tupD
+
+print tupD[3]
+
+print tupD[-3:]
+
+print len(tupD)
+
+dict1=()
+dict1
+
